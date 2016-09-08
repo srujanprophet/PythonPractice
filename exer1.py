605c8e14c9f7b544acb3040fbf62aacb593ddd06
srujanprophet <srujanprophet@gmail.com>
2016-09-08 21:50:56 +0530

    Create exer1.py

---
+++ exer1.py
@@ -0,0 +1,8 @@
+"""Create a program that asks the user to enter their name and their age. 
+Print out a message addressed to them that tells them the year that they will turn 100 years old.
+"""
+
+name = raw_input("Enter your name : ")
+age = int(raw_input("Enter your age : ")
+century = (2016-age) + 100
+print name + '\n, You will turn a 100 years old in the year ',century
