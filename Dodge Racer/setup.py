9079c58330e82fff827724f4ad00c2efdc65a1e1
Srujan Patel <srujanprophet@gmail.com>
2018-01-07 19:48:09 +0530

    Add files via upload

---
+++ Dodge Racer/setup.py
@@ -0,0 +1,10 @@
+import cx_Freeze
+
+executables = [cx_Freeze.Executable("Racey.py")]
+
+cx_Freeze.setup(
+	name="Dodge Racer",
+	options={"build_exe": {"packages":["pygame"],
+						   "include_files":["racecar.png","carIcon.png","Crash.wav","Love_Struck.wav"]}},
+	executables= executables
+	)
