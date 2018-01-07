59d685a5fecada0db53fb2acb757eeb9738d9ce4
Srujan Patel <srujanprophet@gmail.com>
2018-01-07 19:58:53 +0530

    Update setup.py

--- Dodge Racer/setup.py
+++ Dodge Racer/setup.py
@@ -5,6 +5,6 @@ executables = [cx_Freeze.Executable("Racey.py")]
 cx_Freeze.setup(
 	name="Dodge Racer",
 	options={"build_exe": {"packages":["pygame"],
-						   "include_files":["racecar.png","carIcon.png","Crash.wav","Love_Struck.wav"]}},
+						   "include_files":["racecar.png","carIcon.png","Crash.wav","Cha_Cappella.wav"]}},
 	executables= executables
 	)
